c83748530f6cd04c3b26a0c7e92515ed47cff663
Gavin <gad1380@gmail.com>
2018-04-14 00:49:44 -0400

    First Python commit.

---
+++ ex1.py
@@ -0,0 +1,7 @@
+#print "hello world!"
+print "hello again"
+print "i like typing this."
+print "this is funself."
+print 'yay printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
